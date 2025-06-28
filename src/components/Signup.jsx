82e1a6553000baa6b220b8303a2e2641c5a08b77
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2025-06-28 16:36:48 +0100

    fixed font size in explainer and form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -9,7 +9,7 @@ function Signup() {
     <div>
       <div className="form-container">
         <Header hideTicket />
-        <h2 className="font-[FineVintage] text-[48px] md:text-[96px] form-header">
+        <h2 className="font-[FineVintage] text-3xl sm:text-[48px] md:text-[96px] form-header">
           Get Your Ticket
         </h2>
         <Form />
