908216f7f5954c501d54e398acf729180858d39d
fantee-hub <tobifaniran@gmail.com>
2025-06-28 08:50:34 +0100

    fixed form header

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,6 @@
 import Form from "./Form";
 import Footer from "./Footer";
+import formheader from "../../public/form-header.png";
 
 import "./form.css";
 import Header from "./Header";
@@ -7,8 +8,9 @@ import Header from "./Header";
 function Signup() {
   return (
     <div>
-          <div className="form-container">
-              <Header hideTicket/>
+      <div className="form-container">
+        <Header hideTicket />
+        <img src={formheader} alt="" style={{ paddingTop: "10px" }} />
         <Form />
       </div>
       <Footer />
